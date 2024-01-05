1aa5adae716cb5338b15607173b0afec1a4dbe9d
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-06 01:19:54 +0700

    day 04 part 1

--- day04/src/soln.rs
+++ day04/src/soln.rs
@@ -11,7 +11,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn calculate_points(card: &str) -> Result<i32, &'static str> {
+fn parse(card: &str) -> Result<(HashSet<String>, Vec<String>), &'static str> {
     let card = match card.find(':') {
         Some(index) => &card[(index + 1)..],
         None => return Err("Card prefix not found"),
@@ -22,36 +22,43 @@ fn calculate_points(card: &str) -> Result<i32, &'static str> {
         return Err("Invalid card format");
     }
 
-    let winning_numbers =
-        parts[0]
-            .trim()
-            .split_whitespace()
-            .fold(HashSet::new(), |mut accu, num| {
-                accu.insert(num);
-                accu
-            });
+    let winning_numbers = parts[0].trim().split_whitespace().map(str::to_string).fold(
+        HashSet::new(),
+        |mut accu, num| {
+            accu.insert(num);
+            accu
+        },
+    );
 
-    let count = parts[1]
+    let numbers = parts[1]
         .trim()
         .split_whitespace()
-        .filter(|num| winning_numbers.contains(num))
+        .map(str::to_string)
+        .collect::<Vec<String>>();
+
+    Ok((winning_numbers, numbers))
+}
+
+fn calculate_points(parsed_card: &(HashSet<String>, Vec<String>)) -> i32 {
+    let (winning_numbers, numbers) = parsed_card;
+    let count = numbers
+        .iter()
+        .filter(|num| winning_numbers.contains(*num))
         .count() as u32;
 
     if count == 0 {
-        return Ok(0);
+        return 0;
     }
 
-    Ok(2_i32.pow(count - 1))
+    2_i32.pow(count - 1)
 }
 
 pub fn process_part1(filename: &str) -> Option<i32> {
     if let Ok(lines) = read_lines(filename) {
         let sum = lines
             .filter_map(|line_result| line_result.ok())
-            .map(|line| match calculate_points(&line) {
-                Ok(points) => points,
-                Err(_) => 0,
-            })
+            .filter_map(|line| parse(&line).ok())
+            .map(|card| calculate_points(&card))
             .sum();
 
         return Some(sum);
