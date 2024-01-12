cc1238d0309b98737bb22c455eff773ed242ff1b
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-13 00:25:25 +0700

    day 05 part 1

--- day05/src/soln.rs
+++ day05/src/soln.rs
@@ -1,16 +1,56 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::fs::read_to_string;
 
-pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn parse(block: &str) -> Vec<Vec<u32>> {
+    block
+        .split("\n")
+        .skip(1)
+        .filter_map(|line| {
+            let nums = line
+                .trim()
+                .split_whitespace()
+                .filter_map(|v| v.parse().ok())
+                .collect::<Vec<u32>>();
+
+            if nums.is_empty() {
+                None
+            } else {
+                Some(nums)
+            }
+        })
+        .collect()
 }
 
-pub fn process_part1(_filename: &str) -> Option<u32> {
+fn get_input_maps(filename: &str) -> (Vec<u32>, Vec<Vec<Vec<u32>>>) {
+    let contents = read_to_string(filename).expect("should read file");
+
+    let mut seeds: Vec<u32> = Vec::new();
+    let mut maps: Vec<Vec<Vec<u32>>> = Vec::with_capacity(7);
+
+    contents
+        .split("\n\n")
+        .enumerate()
+        .for_each(|(index, block)| {
+            if index == 0 {
+                seeds = block
+                    .trim()
+                    .split_whitespace()
+                    .skip(1)
+                    .filter_map(|v| v.parse().ok())
+                    .collect::<Vec<u32>>();
+            } else {
+                maps.push(parse(block));
+            }
+        });
+
+    (seeds, maps)
+}
+
+pub fn process_part1(filename: &str) -> Option<u32> {
+    let (seeds, maps) = get_input_maps(filename);
+
+    println!("{:?}", seeds);
+    println!("{:?}", maps);
+
     None
 }
 
