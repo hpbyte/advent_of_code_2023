a9196b28f8cc4cfb46308c387c43a1bb380aa42b
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-17 23:36:53 +0700

    day 06 part 1

--- day06/src/soln.rs
+++ day06/src/soln.rs
@@ -1,22 +1,39 @@
 use std::fs::read_to_string;
-use std::io::Error;
 
-fn parse(filename: &str) -> Result<Vec<Vec<u32>>, Error> {
-    let contents = read_to_string(filename)?;
+fn parse(filename: &str) -> Result<Vec<(u32, u32)>, &'static str> {
+    if let Ok(contents) = read_to_string(filename) {
+        let races: Vec<Vec<u32>> = contents
+            .lines()
+            .map(|line| {
+                line.split_whitespace()
+                    .filter_map(|v| v.parse::<u32>().ok())
+                    .collect()
+            })
+            .collect();
 
-    let races: Vec<Vec<u32>> = contents
-        .split("\n")
-        .map(|line| {
-            line.split_whitespace()
-                .filter_map(|word| word.parse::<u32>().ok())
-                .collect()
-        })
-        .collect();
+        if races.len() != 2 {
+            return Err("a race should have time and distance records!");
+        }
 
-    Ok(races)
+        let races = races[0]
+            .iter()
+            .zip(races[1].iter())
+            .map(|(&time, &distance)| (time, distance))
+            .collect();
+
+        return Ok(races);
+    }
+
+    Err("something went wrong during parsing")
 }
 
-pub fn process_part1(_filename: &str) -> Option<u32> {
+pub fn process_part1(filename: &str) -> Option<u32> {
+    if let Ok(parsed) = parse(filename) {
+        println!("{:?}", parsed);
+
+        return Some(0);
+    }
+
     None
 }
 
