0d928a7fe82809cea7d33a4c362fb37a36bf800c
Htoo Pyae <htoopyae.dev@gmail.com>
2024-01-20 15:01:51 +0700

    day 06 part 1

--- day06/src/soln.rs
+++ day06/src/soln.rs
@@ -27,16 +27,32 @@ fn parse(filename: &str) -> Result<Vec<(u32, u32)>, &'static str> {
     Err("something went wrong during parsing")
 }
 
+fn find_total_ways_to_win((race_time, race_distance): (u32, u32)) -> u32 {
+    let mut total_ways = 0;
+
+    for hold_time in 1..race_time {
+        let distance = hold_time * (race_time - hold_time);
+        if distance > race_distance {
+            total_ways += 1;
+        }
+    }
+
+    total_ways
+}
+
 pub fn process_part1(filename: &str) -> Option<u32> {
     if let Ok(parsed) = parse(filename) {
-        println!("{:?}", parsed);
+        let multiplied = parsed
+            .iter()
+            .map(|race| find_total_ways_to_win(*race))
+            .fold(1, |accu, wins| accu * wins);
 
-        return Some(0);
+        return Some(multiplied);
     }
 
     None
 }
 
 pub fn process_part2(_filename: &str) -> Option<u32> {
-    None
+    Some(0)
 }
