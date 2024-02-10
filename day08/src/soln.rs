a45380183f3295c3fbd3455287e8a8c8cd4aed32
Htoo Pyae <htoopyae.dev@gmail.com>
2024-02-10 16:00:29 +0700

    day 08 part 2

--- day08/src/soln.rs
+++ day08/src/soln.rs
@@ -33,7 +33,7 @@ fn parse(filename: &str) -> Result<Map, io::Error> {
     })
 }
 
-fn traverse(map: &Map, start: &str) -> u64 {
+fn traverse_bfs(map: &Map, start: &str) -> u64 {
     let mut curr = start;
     let mut steps: u64 = 0;
     let mut i: usize = 0;
@@ -62,15 +62,7 @@ fn traverse(map: &Map, start: &str) -> u64 {
     steps
 }
 
-fn traverse_multi(map: &Map) -> u64 {
-    map.nodes
-        .keys()
-        .filter(|k| k.ends_with('A'))
-        .map(|node| traverse(map, node))
-        .reduce(lcm)
-        .unwrap_or(0)
-}
-
+// greatest common divisor
 fn gcd(a: u64, b: u64) -> u64 {
     let remainder = a % b;
     if remainder == 0 {
@@ -79,21 +71,43 @@ fn gcd(a: u64, b: u64) -> u64 {
     return gcd(b, remainder);
 }
 
+// least common multiple
 fn lcm(a: u64, b: u64) -> u64 {
     a * (b / gcd(a, b))
 }
 
 pub fn process_part1(filename: &str) -> Option<u64> {
     if let Ok(parsed) = parse(filename) {
-        return Some(traverse(&parsed, "AAA"));
+        return Some(traverse_bfs(&parsed, "AAA"));
     }
 
     None
 }
 
+/**
+ * PART2:
+ * we start from all the nodes that ends with 'A'
+ * and simultaneously traverse until all the paths reach to nodes ends with 'Z'
+ * the tricky part here is that in order for all the paths to finish at the same time,
+ * some of them has to be repeated several times until other paths can catch up.
+ * and these paths are cyclical.
+ * https://www.reddit.com/r/adventofcode/comments/18did3d/2023_day_8_part_1_my_input_maze_plotted_using/
+ *
+ *
+ * here, we don't actually repeat but compute the least common multiple of all the path lengths
+ * and this gives us the one path length
+ */
 pub fn process_part2(filename: &str) -> Option<u64> {
-    if let Ok(parsed) = parse(filename) {
-        return Some(traverse_multi(&parsed));
+    if let Ok(map) = parse(filename) {
+        let steps = map
+            .nodes
+            .keys()
+            .filter(|k| k.ends_with('A'))
+            .map(|node| traverse_bfs(&map, node))
+            .reduce(lcm)
+            .unwrap_or(0);
+
+        return Some(steps);
     }
 
     None
